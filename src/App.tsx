c026650ffc222186d3e0acd0a47515543fd6bb5d
Bruna <brunadias_30@hotmail.com>
2023-04-10 07:57:50 -0300

    criando contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { createContext, useContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  return <ContextoProvider><Rotas /></ContextoProvider>
+
+}
+
+function Rotas() {
   return (
     <>
       <div>
@@ -9,6 +15,7 @@ function App() {
             <Route path="/a" element={<A />} />
             <Route path="/b" element={<B />} />
             <Route path="/c" element={<C />} />
+            <Route path="/d" element={<D />} />
             <Route path="*" element={<Erro />} />
           </Routes>
         </BrowserRouter>
@@ -29,8 +36,33 @@ function C() {
   return <div>Componente C</div>
 }
 
+function D() {
+  return <div>Componente D</div>
+}
+
 function Erro() {
   return <div>Indisponível</div>
 }
 
 export default App;
+
+interface Props {
+  logado: boolean
+  setLogado: Function;
+}
+
+const Contexto = createContext({} as Props);
+
+function ContextoProvider({ children }: any) {
+  const [logado, setLogado] = useState(false);
+  return (
+    <Contexto.Provider value={{ logado, setLogado }}>
+      {children}
+    </Contexto.Provider>
+  )
+}
+
+function useLogado() {
+  const contexto = useContext(Contexto);
+  return Contexto
+}
