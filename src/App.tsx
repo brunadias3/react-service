a45173e36325ec68b2e168833cc4ddaac1a4e458
Bruna <brunadias_30@hotmail.com>
2023-04-10 07:37:18 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        oi
+        <BrowserRouter>
+          <Routes>
+            <Route path="/a" element={<A />} />
+            <Route path="/b" element={<B />} />
+            <Route path="/c" element={<C />} />
+            <Route path="*" element={<Erro />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
+}
+
+function A() {
+  return <div>Componente A</div>
+}
+
+function B() {
+  return <div>Componente B</div>
+}
+
+function C() {
+  return <div>Componente C</div>
+}
+
+function Erro() {
+  return <div>Indisponível</div>
 }
 
 export default App;
